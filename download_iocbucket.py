0bf07f281e113191f6cb1c8c59aab829caa86f93
Beau Taub <beau.taub@gmail.com>
2015-05-19 14:09:54 -0400

    Update download_iocbucket.py
    Adjustments to get script to run.

--- download_iocbucket.py
+++ download_iocbucket.py
@@ -20,6 +20,20 @@ ioc_data_path = '/sec-fs/data/IOCs'
 
 IOC_BUCKET = 'https://www.iocbucket.com'
 
+
+# Andres Andreu - handle IOCs stored in sub directories
+
+def process_sub_dirs(thepath=''):
+    if thepath:
+        lpath = thepath.split('/')
+        # get rid of filename
+        lpath.pop(len(lpath)-1)
+        f_str = ''
+        for l in lpath:
+            f_str += l + '/'
+            if not os.path.isdir(f_str[:-1]):
+                os.mkdir(ioc_data_path +'/'+ f_str[:-1])
+
 def main(ioc_data_path):
     
     # Get a list of the existing IOCs
@@ -27,7 +41,7 @@ def main(ioc_data_path):
     
     # Request a list of the current IOCs on IOCBucket.com
     print "Requesting list of current IOCs on IOCBucket.com"
-    ioc_search_http_request = requests.post(IOC_BUCKET + '/search', {'search':''})
+    ioc_search_http_request = requests.post(IOC_BUCKET + '/search', {'search':''},verify=False)
     
     # Parse the HTML
     soup = BeautifulSoup(ioc_search_http_request.content)
@@ -53,10 +67,15 @@ def main(ioc_data_path):
                 else:
                     ioc_already_exists = False
             # If it does not exist then grab the IOC description page        
+
+            # without below, prog would error out with: 
+            # UnboundLocalError: local variable 'ioc_already_exists' referenced before assignment
+            ioc_already_exists = False
+
             if not ioc_already_exists and not ioc_search_hash in hashes_already_checked:
                 ioc_href = IOC_BUCKET + href
                 print "\nGetting new IOC", ioc_href
-                ioc_http_request = requests.get(ioc_href)
+                ioc_http_request = requests.get(ioc_href,verify=False)
                 # Find the base64 encoded URL for the download if it exists
                 match = re.search("Base64\.decode\('(.{10,100}?)'", ioc_http_request.content, re.DOTALL | re.MULTILINE)
                 if match:
@@ -64,13 +83,18 @@ def main(ioc_data_path):
                     # Decode the IOC download URL
                     real_url = IOC_BUCKET + base64.b64decode(base64d_url)
                     # Grab the IOC file via HTTP
-                    ioc_http_request = requests.get(real_url)
+                    ioc_http_request = requests.get(real_url,verify=False)
                     # Avoid duplication by saving the hash to our check-list
                     hashes_already_checked.append(ioc_search_hash)
                     # If the file actually exists (ecaf59784723054267f5c06f34d5315e2f00cec2 does not)
                     #   then save it to the proper filename
                     if 'content-disposition' in ioc_http_request.headers:
                         ioc_filename = ioc_http_request.headers['content-disposition'].split('; ')[1].replace('filename=', '').replace('"', '')
+
+                        if ioc_filename.find('/') > 1:
+                            print "Making dir for %s "% ioc_filename
+                            process_sub_dirs(ioc_filename)
+
                         if not os.path.isfile(os.path.join(ioc_data_path, ioc_filename)):
                             open(os.path.join(ioc_data_path, ioc_filename), "wb").write(ioc_http_request.content)
                             print "New IOC saved", ioc_filename
